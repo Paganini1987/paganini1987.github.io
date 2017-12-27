60ea7f59e84c5830348fe88ea9b0dd81a604a1f7
Pavel Ivonin <pavelivonin@yandex.ru>
2017-12-27 22:18:02 +0300

    clear

--- js/common.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	function collapse_header() {
-		var header=document.querySelector('#responsive_header');
-
-		header.classList.add('min_header');
-	}
-
-	window.addEventListener('scroll', collapse_header);
-})();
